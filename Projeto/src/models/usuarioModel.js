806b1a9c70025dd7b3261b44e84bb5a3d207bf4d
jardim-ana <ana.jardim@sptech.school>
2025-12-07 15:01:01 -0300

    Adicionado dados ao banco de dados

--- Projeto/src/models/usuarioModel.js
+++ Projeto/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idFuncionario, nome, email, fkFilial, ifnull(nomeFantasia, razaoSocial) empresa, contrato FROM Funcionario join filial on fkFilial = idFilial join empresa on fkEmpresa = idEmpresa WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idFuncionario, nome, email, fkFilial, ifnull(nomeFantasia, razaoSocial) empresa, contrato FROM Funcionario join Filial on fkFilial = idFilial join Empresa on fkEmpresa = idEmpresa WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
